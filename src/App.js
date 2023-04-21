09f85ad3212980e9e85cb2842a52f6ee90e0b9d7
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-21 20:44:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import JoditEditor from "jodit-react";
 
 function App() {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+  const [savedContent, setSavedContent] = useState({});
+  const [inputTagValue, setInputTagValue] = useState("");
+  const [originalKey, setOriginalKey] = useState(null);
+
+  useEffect(() => {
+    const storedContent =
+      JSON.parse(localStorage.getItem("savedContent")) || {};
+    setSavedContent(storedContent);
+  }, []);
+
+  const saveContent = () => {
+    let updatedContent = { ...savedContent };
+
+    if (originalKey !== null && originalKey !== inputTagValue) {
+      delete updatedContent[originalKey];
+    }
+
+    updatedContent[inputTagValue] = content;
+
+    setSavedContent(updatedContent);
+    localStorage.setItem("savedContent", JSON.stringify(updatedContent));
+    setContent("");
+    setInputTagValue("");
+    setOriginalKey(null);
+  };
+
+  const editContent = (key) => {
+    setContent(savedContent[key]);
+    setInputTagValue(key);
+    setOriginalKey(key);
+  };
+
+  const deleteContent = (key) => {
+    const updatedContent = { ...savedContent };
+    delete updatedContent[key];
+    setSavedContent(updatedContent);
+    localStorage.setItem("savedContent", JSON.stringify(updatedContent));
+  };
+
+  const renderSavedContent = () => {
+    return Object.entries(savedContent).map(([key, item]) => (
+      <div key={key}>
+        <h3>{key}</h3>
+        <div dangerouslySetInnerHTML={{ __html: item }}></div>
+        <button onClick={() => editContent(key)}>Edit</button>
+        <button onClick={() => deleteContent(key)}>Delete</button>
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="joditContainer">
+      <JoditEditor
+        ref={editor}
+        value={content}
+        onChange={(newContent) => setContent(newContent)}
+      />
+
+      <label htmlFor="inputTag">Input Tag:</label>
+      <input
+        type="text"
+        id="inputTag"
+        value={inputTagValue}
+        onChange={(e) => setInputTagValue(e.target.value)}
+      />
+
+      <button onClick={saveContent}>
+        {originalKey !== null ? "Update" : "Save"}
+      </button>
+
+      <div>{renderSavedContent()}</div>
     </div>
   );
 }
