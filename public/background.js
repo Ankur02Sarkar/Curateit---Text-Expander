4722959da4a2342a5e3cc08ac8f2b6b1d35d9775
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-23 12:29:10 +0530

    added localstorage + custom links

--- public/background.js
+++ public/background.js
@@ -1,8 +1,18 @@
+const STORAGE_PREFIX = "custom_search_shortcut_";
+
 chrome.omnibox.onInputEntered.addListener((text, disposition) => {
-  if (text === "me") {
-    const url = "https://ankursarkar.vercel.app";
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.update(tabs[0].id, { url });
-    });
-  }
+  chrome.storage.local.get(STORAGE_PREFIX + text, (result) => {
+    const url = result[STORAGE_PREFIX + text];
+    if (url) {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.update(tabs[0].id, { url });
+      });
+    }
+  });
+  // const url = localStorage.getItem(STORAGE_PREFIX + text);
+  // if (url) {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     chrome.tabs.update(tabs[0].id, { url });
+  //   });
+  // }
 });
--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+const STORAGE_PREFIX = "custom_search_shortcut_";
 
 function App() {
+  const [text, setText] = useState("");
+  const [url, setUrl] = useState("");
+
+  const saveShortcut = () => {
+    if (text && url) {
+      // localStorage.setItem(STORAGE_PREFIX + text, url);
+      // setText("");
+      // setUrl("");
+      window.chrome.storage.local.set({ [STORAGE_PREFIX + text]: url }, () => {
+        setText("");
+        setUrl("");
+      });
+    }
+  };
+
   return (
     <div>
       <h1>Custom Search Bar Shortcut</h1>
-      <p>
-        Type "c/me" in the Chrome search bar and hit tab to open the custom
-        link.
-      </p>
+      <p>Type "c:" in the search bar and hit space and enter shortcut</p>
+      <div>
+        <input
+          placeholder="Text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button onClick={saveShortcut}>Save</button>
+      </div>
     </div>
   );
 }
