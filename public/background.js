88baed58fe229190135085cdcc563220d8e524e0
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-04-24 17:40:41 +0530

    added url suggestions

--- public/background.js
+++ public/background.js
@@ -1,5 +1,25 @@
 const STORAGE_PREFIX = "custom_search_shortcut_";
 
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  chrome.storage.local.get(null, (items) => {
+    const savedShortcuts = Object.entries(items)
+      .filter(([key]) => key.startsWith(STORAGE_PREFIX))
+      .map(([key, value]) => ({
+        text: key.replace(STORAGE_PREFIX, ""),
+        url: value,
+      }));
+
+    const suggestions = savedShortcuts
+      .filter((shortcut) => shortcut.text.startsWith(text))
+      .map((shortcut) => ({
+        content: shortcut.text,
+        description: `${shortcut.text} - ${shortcut.url}`,
+      }));
+
+    suggest(suggestions);
+  });
+});
+
 chrome.omnibox.onInputEntered.addListener((text, disposition) => {
   chrome.storage.local.get(STORAGE_PREFIX + text, (result) => {
     const url = result[STORAGE_PREFIX + text];
